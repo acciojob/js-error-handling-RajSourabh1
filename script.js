4d8fb66e880793b3d209b798d3592c848bbbf6f7
Sourabh Kumar <createownpath111@gmail.com>
2023-04-27 13:00:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+let input = document.getElementById("input").value;
+
+class OutOfRangeError extends Error{
+	constructor(arg) {
+		super(`Expression should only consist of integers and +-/* characters and not ${arg}`)
+	    this.name = "OutOfRangeError";
+	}
+}
+
+class InvalidExprError extends Error{
+	constructor() {
+		super(`Expression should not have an invalid combination of expression`)
+	    this.name = "InvalidExprError";
+	}
+}
